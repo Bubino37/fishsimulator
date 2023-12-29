9d47e895e2ded4b738c75d05e3a11c3c8d67272c
Bubino37 <81470308+Bubino37@users.noreply.github.com>
2023-12-29 17:40:08 +0100

    pridanie pozadia a json prvotny navrh

--- script.js
+++ script.js
@@ -1,15 +1,71 @@
-let keyDownTimer;
+let levelsData = null; // Globálna premenná pre údaje o úrovni
+let keyDownTimer; // Timer for keydown event
+let isCastingEnabled = true; // Global variable to manage casting state
+
+// Načítanie hráčových údajov
+async function getPlayerData() {
+  try {
+    const response = await fetch('./data.json');
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Chyba pri načítaní hráčových dát:", error);
+  }
+}
+
+// Aktualizácia hráčových údajov
+async function updatePlayerData(newData) {
+  try {
+    const response = await fetch('./data.json', {
+      method: 'PUT', // alebo 'POST' alebo iný podľa vašej API konfigurácie
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newData),
+    });
+    return response.json();
+  } catch (error) {
+    console.error("Chyba pri aktualizácii hráčových dát:", error);
+  }
+}
+
+// Reset hráčových údajov
+async function resetPlayerData() {
+  const defaultData = {
+    "money": 1000,
+    "inventory": [],
+    "level": 1
+  };
+  await updatePlayerData(defaultData);
+}
+
+// Načítanie údajov o úrovni
+async function loadLevelsData() {
+  try {
+    const response = await fetch('./level.json');
+    levelsData = await response.json();
+    setLevelBackground(levelsData[0]); // Nastavíme defaultne na prvú úroveň, alebo podľa hráčových údajov
+ 
+  } catch (error) {
+    console.error("Chyba pri načítaní údajov o úrovni:", error);
+  }
+}
+
+function setLevelBackground(levelData) {
+    const gameArea = document.getElementById("gameArea");
+    if(gameArea && levelData.image) {
+      gameArea.style.backgroundImage = `url('./${levelData.image}')`;
+    }
+  }
 
 // Klávesový vstup pre počítače
 document.addEventListener('keydown', (event) => {
-  if (event.key === "w" || event.key === "W") {
+  if ((event.key === "w" || event.key === "W") && isCastingEnabled) {
     // Začína odpočítavanie
     if(!keyDownTimer){
       keyDownTimer = setTimeout(() => {
-        if(isCastingEnabled){
-          showCastButton();
-          isCastingEnabled = false;
-        }
+        showCastButton();
+        isCastingEnabled = false; // Disable further casting until reset
       }, 5000); // 5 sekúnd
     }
   }
@@ -23,8 +79,6 @@ document.addEventListener('keyup', (event) => {
   }
 });
 
-let isCastingEnabled = true;
-
 // Gyroskop pre mobily
 window.addEventListener('deviceorientation', (event) => {
   let beta = event.beta; // Beta je rotácia okolo osi X, ktorá meria náklon hore a dole.
@@ -36,22 +90,33 @@ window.addEventListener('deviceorientation', (event) => {
 
 function showCastButton() {
   const castButton = document.getElementById("castButton");
-  castButton.style.display = "block"; // Zobrazí tlačidlo
+  if (castButton) {
+    castButton.style.display = "block"; // Zobrazí tlačidlo
+  }
 }
 
 function castLine() {
   const castButton = document.getElementById("castButton");
-  castButton.style.display = "none"; // Skryje tlačidlo
-  isCastingEnabled = true; // Povolí detekciu naklonenia
+  if (castButton) {
+    castButton.style.display = "none"; // Skryje tlačidlo
+  }
+  isCastingEnabled = true; // Enable casting again
 }
 
-// Inicializácia a možné nastavenie hry
-function initGame() {
-  const castButton = document.getElementById("castButton");
-  castButton.addEventListener('click', castLine); // Pridáva udalosť pre kliknutie
-  castButton.style.display = "none"; // Skryje tlačidlo na začiatku
+async function initGame() {
+    await loadLevelsData();  // Načítanie údajov o úrovni
+    const playerData = await getPlayerData(); // Získanie údajov o hráči
+    const levelInfo = levelsData.find(level => level.level === playerData.level);
+    if(levelInfo) {
+      setLevelBackground(levelInfo);
+    }
+    console.log(playerData);
+    console.log(levelsData); // Zobraziť údaje o úrovni
+    const castButton = document.getElementById("castButton");
+    if (castButton) {
+      castButton.addEventListener('click', castLine); // Pridáva udalosť pre kliknutie
+      castButton.style.display = "none"; // Skryje tlačidlo na začiatku
+    }
 }
-
-// Volanie inicializácie pri načítaní
+  
 initGame();
-
