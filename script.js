e8dc7594c1c6ade389abf0fe4f1f6f92472daf5b
Bubino37 <81470308+Bubino37@users.noreply.github.com>
2024-01-03 17:07:13 +0100

    fixnutie hry, pridanie shopu a button menu

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
 let levelsData = null; // Globálna premenná pre údaje o úrovni
+let playerData = null; // Globálna premenná pre údaje hráča
 let keyDownTimer; // Timer for keydown event
 let fishCatchTimer; // Timer for catching fish
 let catchTime; // This will be a random time between 10 and 30 seconds
 let isCastingEnabled = true; // Global variable to manage casting state
+let isGamePaused = false;
+let timerStart = null; // Start time of the fish catch timer
+let remainingCatchTime = null;
+
 
 // Načítanie hráčových údajov
 async function getPlayerData() {
   try {
-    const response = await fetch('./data.json');
-    const data = await response.json();
-    return data;
+      const response = await fetch('./data.json');
+      const data = await response.json();
+      return data;
   } catch (error) {
-    console.error("Chyba pri načítaní hráčových dát:", error);
+      console.error("Chyba pri načítaní hráčových dát:", error);
   }
 }
+
 async function loadPlayerData() {
-  playerData = await getPlayerData(); // Loads data into the playerData variable
+  playerData = await getPlayerData(); // Load data
+  if (!playerData.purchasedLevels) {
+      playerData.purchasedLevels = []; // Initialize if not present
+  }
   displayPlayerData(playerData);
 }
 // Aktualizácia hráčových údajov
 async function updatePlayerData(newData) {
-  try {
-    const response = await fetch('http://localhost:3000/update', {
-      method: 'POST', // Zmena na POST
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newData),
-    });
-    const updatedData = await response.json();
-    displayPlayerData(updatedData.data); // Aktualizuje zobrazenie s novými údajmi
-    return updatedData;
-  } catch (error) {
-    console.error("Chyba pri aktualizácii hráčových dát:", error);
-  }
+  playerData = newData; // Updates data in the playerData variable
+  displayPlayerData(playerData); // Displays updated data
 }
 
 function showMenu() {
+  pauseGame(); // Pozastavi hru
   document.getElementById('menu').style.display = 'flex';
 }
 
@@ -58,36 +56,27 @@ window.onclick = function(event) {
     hideGameControlsModal();
   }
 };
-
 async function catchFish() {
   clearTimeout(fishCatchTimer); // Stop the timer as fish is caught
-  const currentLevel = playerData.level; // Or other way to get the player's level
+  remainingCatchTime = null; // Reset remaining time
+    timerStart = null; // Reset timer start
+  const currentLevel = playerData.level;
   const levelInfo = levelsData.find(level => level.level === currentLevel);
 
-  // Adds fish to inventory
   if (levelInfo) {
       const newFish = {
           species: levelInfo["ryba-druh"],
           value: levelInfo.cena
       };
 
-      // Checks if the fish of the same species already exists in the inventory
       let fishIndex = playerData.inventory.findIndex(fish => fish.species === newFish.species);
       if (fishIndex !== -1) {
-          // Adds the value to the existing item
           playerData.inventory[fishIndex].value += newFish.value;
       } else {
-          // Adds the new fish to inventory
           playerData.inventory.push(newFish);
       }
-
-      // Updates player data
+      isCastingEnabled = true;
       await updatePlayerData(playerData);
-
-      // Updates display of player data
-      displayPlayerData(playerData);
-
-      isCastingEnabled = true; // Enables casting again for next round
   }
 }
 
@@ -137,7 +126,6 @@ async function resetPlayerData() {
   };
   await updatePlayerData(defaultData);
 }
-
 // Načítanie údajov o úrovni
 async function loadLevelsData() {
   try {
@@ -151,25 +139,27 @@ async function loadLevelsData() {
 }
 
 function prepareToCatchFish() {
-  console.log("Udica sa hodila!"); // Potvrdiť v konzole, že udica bola hodena
-  isCastingEnabled = false; // Zastaví opätovné hodenie, kým sa nevykoná akcia chytenia alebo resetu
+  if (isCastingEnabled) {
+    console.log("Udica sa hodila!");
+  isCastingEnabled = false;
 
-  // Vygeneruje náhodný čas (medzi 10 a 30 sekundami) a potom zobrazí tlačidlo na chytenie ryby
   catchTime = Math.floor(Math.random() * (30000 - 10000 + 1) + 10000);
-  setTimeout(showCatchButton, catchTime);
+  timerStart = new Date().getTime(); // Set the start time for the timer
+  fishCatchTimer = setTimeout(showCatchButton, catchTime);
+  }
+  
 }
-
 function setLevelBackground(levelData) {
     const gameArea = document.getElementById("gameArea");
     if(gameArea && levelData.image) {
       gameArea.style.backgroundImage = `url('./${levelData.image}')`;
     }
   }
-
 // Klávesový vstup pre počítače
 document.addEventListener('keydown', (event) => {
   if ((event.key === "w" || event.key === "W") && isCastingEnabled) {
     keyDownTimer = setTimeout(prepareToCatchFish, 5000); // 5 second keydown to cast line
+    
   }
 });
 
@@ -178,7 +168,6 @@ document.addEventListener('keyup', (event) => {
     clearTimeout(keyDownTimer); // Cancel the countdown if key is released early
   }
 });
-
 // Gyroskop pre mobily
 window.addEventListener('deviceorientation', (event) => {
   let beta = event.beta; // Beta je rotácia okolo osi X, ktorá meria náklon hore a dole.
@@ -188,29 +177,101 @@ window.addEventListener('deviceorientation', (event) => {
     isCastingEnabled = false; // Zastaviť detekciu naklonenia kým sa neklikne na tlačidlo
   }
 });
-
 function showCastButton() {
   const castButton = document.getElementById("castButton");
   if (castButton) {
     castButton.style.display = "block"; // Zobrazí tlačidlo
   }
 }
+function populateShop() {
+  const shopItems = document.getElementById('shopItems');
+  shopItems.innerHTML = ''; // Clear existing items
 
-function showCatchButton() {
-  const castButton = document.getElementById("castButton");
-  if (castButton) {
-      castButton.style.display = "block"; // Zobrazí tlačidlo na chytenie ryby
-      console.log("Môžeš chytiť rybu!"); // Informuje v konzole
+  levelsData.forEach(level => {
+      let li = document.createElement('li');
+      li.textContent = `${level.info.charAt(0).toUpperCase() + level.info.slice(1)} - Cena: ${level.requiredExperience} skúsenostných bodov`;
 
-      // Nastaví časovač na skrytie tlačidla na chytenie ryby (po 3 sekundách)
-      setTimeout(() => {
-          castButton.style.display = "none";
-          console.log("Ryba ušla!"); // Informuje v konzole, ak hráč nestihne kliknúť
-          isCastingEnabled = true; // Umožní opätovné hodenie udice
-      }, 3000);
+      let actionButton = document.createElement('button');
+      if (playerData.purchasedLevels.includes(level.level)) {
+          if(playerData.level === level.level){
+              actionButton.textContent = 'Aktuálny';
+              actionButton.disabled = true;
+          } else {
+              actionButton.textContent = 'Prepnúť';
+              actionButton.onclick = function() { switchLevel(level); };
+          }
+      } else {
+          actionButton.textContent = 'Kúpiť';
+          actionButton.onclick = function() { buyLevel(level); };
+      }
+      li.appendChild(actionButton);
+      shopItems.appendChild(li);
+  });
+}
+function buyLevel(level) {
+  if(playerData.money >= level.requiredExperience && !playerData.purchasedLevels.includes(level.level)) {
+      playerData.money -= level.requiredExperience; // Subtract the cost
+      playerData.level = level.level; // Update player's level
+      playerData.purchasedLevels.push(level.level); // Mark level as purchased
+
+      setLevelBackground(level); // Update background
+      updatePlayerData(playerData); // Update display and data
+
+      populateShop(); // Refresh the shop display
+      console.log(`${level.info} zakúpený!`);
+  } else {
+      console.log("Nedostatočné finančné prostriedky alebo úroveň už bola zakúpená!");
   }
 }
 
+function switchLevel(level) {
+  if (playerData.purchasedLevels.includes(level.level)) {
+      playerData.level = level.level; // Switch to the new level
+      setLevelBackground(level); // Update background
+      updatePlayerData(playerData); // Update display and data
+      console.log(`Prepnuté na ${level.info}.`);
+  } else {
+      console.log("Úroveň ešte nebola zakúpená!");
+  }
+}
+
+function toggleShop() {
+  let shopModal = document.getElementById('shopModal');
+  if(shopModal.style.display === 'none') {
+      shopModal.style.display = 'block';
+      populateShop(); // Populate the shop with items when opened
+  } else {
+      shopModal.style.display = 'none';
+  }
+}
+
+function toggleMenu() {
+  if (document.getElementById('menu').style.display === 'flex') {
+      resumeGame();
+      document.getElementById('menu').style.display = 'none';
+  } else {
+      showMenu();
+  }
+}
+function showCatchButton() {
+  if (!isGamePaused) {
+      const castButton = document.getElementById("castButton");
+      if (castButton) {
+          castButton.style.display = "block"; // Zobrazí tlačidlo na chytenie ryby
+          console.log("Môžeš chytiť rybu!");
+
+          fishCatchTimer = setTimeout(() => {
+              if (!isGamePaused) { // Check if game is not paused
+                  castButton.style.display = "none";
+                  console.log("Ryba ušla!");
+                  isCastingEnabled = true; // Umožní opätovné hodenie udice
+                  remainingCatchTime = null; // Reset remaining time
+                  timerStart = null; // Reset timer start
+              }
+          }, 3000);
+      }
+  }
+}
 function castLine() {
   const castButton = document.getElementById("castButton");
   if (castButton) {
@@ -222,12 +283,32 @@ function castLine() {
   catchTime = (Math.random() * (30000 - 10000) + 10000).toFixed(0);
   console.log(catchTime);
   // Po uplynutí náhodného času sa zobrazí tlačidlo na chytenie ryby na 3 sekundy
-  fishCatchTimer = setTimeout(showCatchButton, catchTime);
+  // fishCatchTimer = setTimeout(showCatchButton, catchTime);
   
 }
 
+function pauseGame() {
+  console.log("Hra je zastavena.");
+  if (fishCatchTimer) {
+      clearTimeout(fishCatchTimer); // Clears the existing fish catch timer
+      // Calculate remaining time for catch if the timer was running
+      if (timerStart) {
+          remainingCatchTime = catchTime - (new Date().getTime() - timerStart);
+      }
+  }
+  isGamePaused = true;
+}
+function resumeGame() {
+  console.log("Hra pokracuje.");
+    if (remainingCatchTime && remainingCatchTime > 0) {
+        fishCatchTimer = setTimeout(showCatchButton, remainingCatchTime);
+        timerStart = new Date().getTime(); // Reset the start time for the timer
+    }
+    isGamePaused = false;
+}
 async function startGame() {
-  hideMenu(); // Skryje menu
+  hideMenu();
+    resumeGame();  // Pridané na obnovenie hry
   // Príprava herného prostredia
   // Tu môžete napríklad resetovať skóre, nastaviť alebo skryť/vykresliť herné prvky, prípadne načítať herné úrovne
   // Tu by ste tiež mali obnoviť príznak, aby sa umožnilo hodenie udice
@@ -259,7 +340,6 @@ document.addEventListener('DOMContentLoaded', function() {
     castButton.style.display = "none"; // Hides the button at the start
   }
 });
-
 async function initGame() {
   await loadLevelsData();
   await loadPlayerData(); // This function will load data and store it in playerData
